5acd2bfaa55f87d7640342ca0d9bdf8209ddeccf
Toty-01 <caucheanthony@gmail.com>
2023-02-08 11:02:30 +0100

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import {Loader, Card, FormField} from '../components'
+import "./style.css"
 
 const RenderCards = ({data, title}) => {
   if(data?.length > 0) {
